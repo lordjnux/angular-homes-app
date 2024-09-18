c743860e333c03f8dd16da8e1cbf404bce73146e
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-09-18 16:16:47 -0500

    feat: deatails finished - part6: services

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -8,7 +8,34 @@ import { HousingLocation } from "../housing-location";
   selector: "app-details",
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>details works! {{ houseLocation?.id }}</p> `,
+  template: `
+    <article>
+      <img
+        class="listing-photo"
+        [src]="houseLocation?.photo"
+        alt="Exterior
+      photo of {{ houseLocation?.name }}"
+      />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ houseLocation?.name }}</h2>
+        <p class="listing-location">
+          {{ houseLocation?.city }}, {{ houseLocation?.state }}
+        </p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Available units: {{ houseLocation?.availableUnits }}</li>
+          <li>Laundry: {{ houseLocation?.laundry }}</li>
+          <li>WiFi: {{ houseLocation?.wifi }}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <button class="primary" type="button">Apply now</button>
+      </section>
+    </article>
+  `,
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
